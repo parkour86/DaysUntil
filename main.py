9dbc98bdc77a1793da5f05a5727931b9d554d7a1
Roku <parkour86@gmail.com>
2025-06-22 15:13:39 -0700

    .

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ class DaysUntilAction(ActionBase):
             f"{lm.get('actions.daysuntil.name')}\n{display_date}",
             font_size=int(lm.get("actions.daysuntil.toplabel.font", 14)),
             color=[172, 244, 188],  # #ACF4BC
-            font_family="UmpushBold",
+            font_family="Umpush:style=Bold",
             update=True
         )
         color = [253, 195, 123]  # #FDC37B
@@ -102,7 +102,7 @@ class DaysUntilAction(ActionBase):
             label = "\n--"
             font_size = 22
 
-        self.set_center_label(label, font_size=font_size, font_family="UmpushBold", color=color, update=True)
+        self.set_center_label(label, font_size=font_size, font_family="Umpush:style=Bold", color=color, update=True)
 
     def calculate_days_until(self, date_str):
         try:
